a9297e235d19ef9c2a1ea3bfda466c5c564b0fcd
dddd-onigiri <dddd.sailing470@gmail.com>
2023-03-09 22:37:42 +0900

    誤字修正

--- pages/00_鍵生成.py
+++ pages/00_鍵生成.py
@@ -24,7 +24,7 @@ q = st.selectbox("素数 q を選択してください", sq)
 
 if p == q:
     st.write(
-        '<span style="color:red">【エラー】p と q が同じ数字のため、暗号化を実行できません。pとq'
+        '<span style="color:red">【エラー】p と q が同じ数字のため、鍵生成を実行できません。pとq'
         'は別々の数字にしてください。</span>',
         unsafe_allow_html=True)
 else:
