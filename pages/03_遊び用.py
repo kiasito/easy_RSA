b9be9d33027d7010d8474eed742ee6e478e71654
itoudaiki <dddd.sailing470@gmail.com>
2023-08-01 20:59:10 +0900

    修正

--- pages/03_遊び用.py
+++ pages/03_遊び用.py
@@ -1,41 +1,41 @@
 import streamlit as st
 import rsa
 
-st.title("RSA Encryption Web Application")
+st.title("RSA暗号化Webアプリケーション")
 
-# Generate RSA keys
+# RSA鍵の生成
 def generate_keys():
     (pubkey, privkey) = rsa.newkeys(512)
     return pubkey, privkey
 
-# Encrypt message
+# メッセージの暗号化
 def encrypt_message(message, pubkey):
     encrypted_message = rsa.encrypt(message.encode(), pubkey)
     return encrypted_message
 
-# Decrypt message
+# メッセージの復号化
 def decrypt_message(encrypted_message, privkey):
     decrypted_message = rsa.decrypt(encrypted_message, privkey)
     return decrypted_message.decode()
 
-# Interface to get user input
-message = st.text_input("Enter your message here")
+# ユーザー入力のインターフェース
+message = st.text_input("ここにメッセージを入力してください")
 
-if st.button('Generate Keys'):
+if st.button('鍵の生成'):
     pubkey, privkey = generate_keys()
-    st.write("Public Key: ", pubkey)
-    st.write("Private Key: ", privkey)
+    st.write("公開鍵: ", pubkey)
+    st.write("秘密鍵: ", privkey)
 
-if st.button('Encrypt Message'):
+if st.button('メッセージの暗号化'):
     if message and pubkey:
         encrypted_message = encrypt_message(message, pubkey)
-        st.write("Encrypted Message: ", encrypted_message)
+        st.write("暗号化されたメッセージ: ", encrypted_message)
     else:
-        st.write("Please generate keys and enter a message")
+        st.write("鍵を生成し、メッセージを入力してください")
 
-if st.button('Decrypt Message'):
+if st.button('メッセージの復号化'):
     if encrypted_message and privkey:
         decrypted_message = decrypt_message(encrypted_message, privkey)
-        st.write("Decrypted Message: ", decrypted_message)
+        st.write("復号化されたメッセージ: ", decrypted_message)
     else:
-        st.write("Please generate keys, enter a message and encrypt it")
+        st.write("鍵を生成し、メッセージを入力し、それを暗号化してください")
